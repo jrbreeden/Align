e130f719e775d75606db63596951e935c4dd396f
Jhimson <jhimsonrayraboypamisa@gmail.com>
2022-07-02 06:32:19 -0400

    Updated design of SignUpForm

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -23,7 +23,7 @@ export default function SignUpForm({ setUser }) {
       setUser(user);
     } catch {
       // An error occurred
-      setData({ ...data, error: 'Sign Up Failed - Try Again' });
+      setData({ ...data, error: 'Sign Up Failed - Try Again!' });
     }
   };
 
@@ -40,7 +40,9 @@ export default function SignUpForm({ setUser }) {
       <div className="bg-grey-lighter min-h-screen flex flex-col">
         <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
           <div className="bg-gray-100 px-6 py-8 rounded shadow-md text-black w-full">
-            <p className="error-message">&nbsp;{data.error}</p>
+            <p className="error-message  mb-4 text-center bg-red-400 p-2 text-white rounded font-bold">
+              &nbsp;{data.error}
+            </p>
             <h1 className="mb-8 text-3xl text-center">Sign up</h1>
             <form onSubmit={handleSubmit}>
               <input
@@ -81,6 +83,7 @@ export default function SignUpForm({ setUser }) {
               <button
                 type="submit"
                 className="w-full text-center py-3 rounded bg-green-600 text-white hover:bg-green-dark focus:outline-none my-1 font-bold"
+                disabled={disable}
               >
                 Create Account
               </button>
@@ -105,7 +108,7 @@ export default function SignUpForm({ setUser }) {
           </div>
 
           <div className="text-grey-dark mt-6">
-            Already have an account?
+            Already have an account?{' '}
             <a
               className="no-underline border-b border-blue text-blue"
               href="../login/"
