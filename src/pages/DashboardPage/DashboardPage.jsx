35bb0b0d96103dda1259f3b31a2bd238044f470d
Jhimson <jhimsonrayraboypamisa@gmail.com>
2022-07-02 16:31:21 -0400

    Added logout functionality for Dashboard Page

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { logOut } from '../../utilities/users-service';
+import { useNavigate } from 'react-router-dom';
 
-export default function Dashboard({ setUser }) {
-  return <div>Dashboard</div>;
+export default function Dashboard({ user, setUser }) {
+  const navigate = useNavigate();
+  const handleLogOut = () => {
+    logOut();
+    setUser(null);
+    navigate('/login')
+  };
+  return (
+    <>
+      <div>Welcome, {user.name}</div>
+      <button className="bg-red-400 px-4 py-2" onClick={handleLogOut}>
+        Logout
+      </button>
+    </>
+  );
 }
