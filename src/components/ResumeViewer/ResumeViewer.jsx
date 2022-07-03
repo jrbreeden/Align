5475f269759621b8ae7dd4b051374f1626127f78
Jhimson <jhimsonrayraboypamisa@gmail.com>
2022-07-03 18:03:51 -0400

    Create ResumeViewer component

---
+++ src/components/ResumeViewer/ResumeViewer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ResumeViewer() {
+  return (
+    <div className='h-96 bg-gray-400 p-8 border border-2 border-black w-96'>ResumeViewer</div>
+  )
+}
