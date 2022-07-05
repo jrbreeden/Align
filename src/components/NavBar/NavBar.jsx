5a16268c44452d44d39d51fe5d7e71932f171ca4
bhenderson93 <brycehenderson1993@gmail.com>
2022-07-05 10:47:18 -0700

    Syncing all group members

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -52,13 +52,8 @@ export default function NavBar({ active, user }) {
             <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex-shrink-0 flex items-center">
                 <img
-                  className="block lg:hidden h-8 w-auto"
-                  src={Logo}
-                  alt="Workflow"
-                />
-                <img
                   className="hidden lg:block h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                  src={Logo}
                   alt="Workflow"
                 />
               </div>
