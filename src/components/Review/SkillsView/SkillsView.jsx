6d2ca9458f6946ef2c84c51a40706c753b0fc65c
Jhimson <jhimsonrayraboypamisa@gmail.com>
2022-07-05 05:55:09 -0400

    Delete skill from skill view functional

--- src/components/Review/SkillsView/SkillsView.jsx
+++ src/components/Review/SkillsView/SkillsView.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
 
-export default function SectionInput({ section, skills }) {
+export default function SkillsView({ section, skills, setSkills }) {
   const checkPriority = (priority) => {
     switch (priority) {
       case 'normal':
         return '[#169ed9]';
 
       case 'essential':
-        return '[#ffd650]';
+        return '[#FFB627]';
 
       case 'high':
         return '[#FF4000]';
@@ -32,14 +33,24 @@ export default function SectionInput({ section, skills }) {
         <div className="items">
           <div className="font-sans flex items-center justify-center bg-blue-darker w-full py-8 drop-shadow-2xl">
             <div className="overflow-hidden bg-white rounded max-w-xs w-full shadow-lg leading-normal">
-              {skills?.map(({id, skill, priority }) => (
+              {skills?.map(({ id, skill, priority }) => (
                 <div
                   key={id}
                   className={`block group bg-${checkPriority(
                     priority
-                  )} p-4 border-b`}
+                  )} p-4 border-b flex justify-between items-center`}
                 >
                   <p className="font-bold text-sm text-black ">{skill}</p>
+                  <button
+                    className="text-black hover:text-white font-bold"
+                    onClick={() => {
+                      setSkills((prevSkills) =>
+                        [...prevSkills].filter((skill) => skill.id !== id)
+                      );
+                    }}
+                  >
+                    <AiOutlineClose />
+                  </button>
                 </div>
               ))}
             </div>
