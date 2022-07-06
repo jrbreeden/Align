7a6e5af7685b6b045b3774a5ea6d2cc7ea656231
Jhimson <jhimsonrayraboypamisa@gmail.com>
2022-07-06 13:42:38 -0400

    Re-organized appliedJobs model

--- models/M-appliedJobs.js
+++ models/M-appliedJobs.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const appliedJobsSchema = new Schema(
-  {
-    user: { type: Schema.Types.ObjectId, ref: 'User' },
-    appliedJobList:[jobDetails]
-  },
-  { timestamps: true }
-);
-
 const jobDetails = new Schema({
   position: { type: String, required: true },
   job_link: { type: String, required: true },
   resume_link: { type: String, required: true },
   job_date_posted: { type: Date, required: true },
-  date_applied: { type: Date, required: false, default:null},
-})
+  date_applied: { type: Date, required: false, default: null },
+});
+
+const appliedJobsSchema = new Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    appliedJobList: [jobDetails],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('AppliedJobs', appliedJobsSchema);
