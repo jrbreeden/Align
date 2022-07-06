655d26a25596949f38cbc09c51b3ca8148da6dd0
Jhimson <jhimsonrayraboypamisa@gmail.com>
2022-07-06 05:07:43 -0400

    Restructured the form layout and view for project section

--- src/components/Review/ProjectsView/ProjectsView.jsx
+++ src/components/Review/ProjectsView/ProjectsView.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
-
 export default function ProjectsView({
   section,
   project: { header, subSections },
-  projects
+  projects,
 }) {
   return (
     <div className="h-auto w-96 min-h-full bg-gray-200 p-8 border border-2 border-gray-300 drop-shadow-2xl rounded">
@@ -13,17 +11,32 @@ export default function ProjectsView({
             {section.toUpperCase()} DATA
           </li>
         </ul>
-        <div className="font-sans flex items-center justify-center bg-blue-darker w-full py-8 drop-shadow-2xl">
-          <div className="overflow-hidden bg-white rounded max-w-xs w-full shadow-lg  leading-normal bg-red-400 cursor-pointer" onClick={() => {
-            console.log('Test')
-          }}>
-            <div className="block group hover:bg-blue p-4 border-b">
-              <p className="font-bold text-lg mb-1 text-black ">{header}</p>
-              {subSections?.length !== 0 && (
-                <p className="text-grey-darker mb-2 ">{`test`}</p>
-              )}
-            </div>
-          </div>
+        <div className="font-sans flex flex-col items-center justify-center bg-blue-darker w-full py-8 drop-shadow-2xl gap-y-4">
+          {projects?.length !== 0 &&
+            projects?.map((project) => (
+              <div
+                className="overflow-hidden bg-white rounded max-w-xs w-full shadow-lg  leading-normal bg-red-400 cursor-pointer"
+                onClick={() => {
+                  console.log(project);
+                }}
+              >
+                <div className="block group hover:bg-blue p-4 border-b">
+                  <p className="font-bold text-lg mb-1 text-black ">
+                    {project.subHeader}
+                  </p>
+                  <div className="flex justify-center">
+                    <ul className="bg-white rounded-lg border border-gray-200 w-96 text-gray-900">
+                      {project?.lineItems?.length !== 0 &&
+                        project?.lineItems?.map((item) => (
+                          <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                            - {item}
+                          </li>
+                        ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            ))}
         </div>
       </div>
     </div>
--- src/components/Sections/ProjectsSection/ProjectSection.jsx
+++ src/components/Sections/ProjectsSection/ProjectSection.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import { Spring, animated } from 'react-spring';
 import ProjectsView from '../../Review/ProjectsView/ProjectsView';
 
@@ -6,17 +6,21 @@ export default function ProjectsSection({
   section,
   project,
   setProject,
+  projects,
+  setProjects,
   projectSubSection,
   setProjectSubSection,
   projectLineItem,
   setProjectLineItem,
 }) {
-  const handleChange = (e) => {
-    setProject((prevProject) => ({
-      ...prevProject,
-      [e.target.name]: e.target.value,
-    }));
-  };
+  const [showLineItemInput, setShowLineItemInput] = useState(false);
+  const [lineItem, setLineItem] = useState('');
+  // const handleChange = (e) => {
+  //   setProject((prevProject) => ({
+  //     ...prevProject,
+  //     [e.target.name]: e.target.value,
+  //   }));
+  // };
   return (
     <Spring
       from={{ opacity: 0, marginLeft: -1000 }}
@@ -40,28 +44,152 @@ export default function ProjectsSection({
                     className="block text-gray-700 text-sm font-bold mb-2"
                     htmlFor="header"
                   >
-                    Header
+                    Sub Section
                   </label>
                   <input
                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     name="header"
-                    value={project.header}
-                    onChange={handleChange}
+                    value={projectSubSection.subHeader}
+                    onChange={(e) =>
+                      setProjectSubSection((prevState) => ({
+                        ...prevState,
+                        subHeader: e.target.value,
+                      }))
+                    }
                     id="header"
                     type="text"
-                    placeholder="Enter Header"
+                    placeholder="Enter Sub Section"
                   />
-                </div>
+                  <label
+                    className="block text-gray-700 text-sm font-bold mb-2 mt-4"
+                    htmlFor="header"
+                  >
+                    Date Start
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    name="dateStart"
+                    value={projectSubSection.dateStart}
+                    onChange={(e) =>
+                      setProjectSubSection((prevState) => ({
+                        ...prevState,
+                        dateStart: e.target.value,
+                      }))
+                    }
+                    id="dateStart"
+                    type="date"
+                  />
 
-                <button
-                  className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  type="button"
-                >
-                  Submit
-                </button>
+                  <label
+                    className="block text-gray-700 text-sm font-bold mb-2 mt-4"
+                    htmlFor="header"
+                  >
+                    Date End
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    name="dateEnd"
+                    value={projectSubSection.dateEnd}
+                    onChange={(e) =>
+                      setProjectSubSection((prevState) => ({
+                        ...prevState,
+                        dateEnd: e.target.value,
+                      }))
+                    }
+                    id="dateEnd"
+                    type="date"
+                  />
+                  {projectSubSection?.lineItems?.length > 0 && (
+                    <div class="flex justify-center mt-4">
+                      <ul class="bg-white rounded-lg border border-gray-200 w-96 text-gray-900">
+                        {projectSubSection?.lineItems?.map((item) => (
+                          <li class="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                            {item}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  )}
+
+                  {showLineItemInput && (
+                    <div>
+                      <label
+                        className="block text-gray-700 text-sm font-bold mb-2 mt-4"
+                        htmlFor="header"
+                      >
+                        Line Item
+                      </label>
+                      <textarea
+                        className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
+                        name="lineItem"
+                        value={lineItem}
+                        onChange={(e) => setLineItem(e.target.value)}
+                        rows="4"
+                        placeholder="Enter Line item"
+                      ></textarea>
+                    </div>
+                  )}
+                </div>
+                {showLineItemInput ? null : (
+                  <button
+                    className="w-full bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="button"
+                    onClick={() => setShowLineItemInput(true)}
+                  >
+                    Add New Line Item
+                  </button>
+                )}
+                {showLineItemInput && (
+                  <button
+                    className="w-full bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="button"
+                    onClick={() => {
+                      setShowLineItemInput(false);
+                      setProjectSubSection((prevState) => ({
+                        ...prevState,
+                        lineItems: [...prevState.lineItems, lineItem],
+                      }));
+                      setLineItem('');
+                    }}
+                  >
+                    Submit Line Item
+                  </button>
+                )}
+                {showLineItemInput ? null : (
+                  <button
+                    className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4"
+                    type="button"
+                    onClick={() => {
+                      setProject((prevProject) => ({
+                        ...prevProject,
+                        header: project.header,
+                        subSections: projectSubSection,
+                      }));
+
+                      setProjects((prevProjects) => [
+                        ...prevProjects,
+                        projectSubSection,
+                      ]);
+                      console.log(project.header);
+                      // Reset ProjectSubSection
+                      setProjectSubSection({
+                        subHeader: '',
+                        dateStart: '',
+                        dateEnd: '',
+                        lineItems: [],
+                      });
+                    }}
+                  >
+                    Submit
+                  </button>
+                )}
               </div>
             </div>
-            <ProjectsView section={section} project={project}/>
+            <ProjectsView
+              section={section}
+              project={project}
+              projects={projects}
+            />
           </div>
         </animated.div>
       )}
