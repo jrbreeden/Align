d72af723fc400a349ddcead397516fa1f84a6ee0
bhenderson93 <brycehenderson1993@gmail.com>
2022-07-06 10:27:34 -0700

    after lunch commit

--- models/M-appliedJobs.js
+++ models/M-appliedJobs.js
@@ -4,13 +4,17 @@ const Schema = mongoose.Schema;
 const appliedJobsSchema = new Schema(
   {
     user: { type: Schema.Types.ObjectId, ref: 'User' },
-    position: { type: String, required: true },
-    job_link: { type: String, required: true },
-    resume_link: { type: String, required: true },
-    job_date_posted: { type: Date, required: true },
-    date_applied: { type: Date, required: true },
+    appliedJobList:[jobDetails]
   },
   { timestamps: true }
 );
+
+const jobDetails = new Schema({
+  position: { type: String, required: true },
+  job_link: { type: String, required: true },
+  resume_link: { type: String, required: true },
+  job_date_posted: { type: Date, required: true },
+  date_applied: { type: Date, required: false, default:null},
+})
 
 module.exports = mongoose.model('AppliedJobs', appliedJobsSchema);
