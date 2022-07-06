fce655affb4a0b5622f8d5595e7101e39255fc66
Jhimson <jhimsonrayraboypamisa@gmail.com>
2022-07-06 05:07:01 -0400

    Re-organized resume model

--- models/M-resume.js
+++ models/M-resume.js
@@ -8,57 +8,87 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const resumeSchema = new Schema({
-    personal: personalSchema,
-    statement: {type:statementSchema, required:false},
-    skills:{type:[{type:skillSchema , required:false , min:5 , max:20}], validate:[50 , 'Too many skills. Max 50']},
-    projects:sectionSchema,
-    workHistory:sectionSchema,
-    education:sectionSchema,
-})
-
 const personalSchema = new Schema({
-    name:{type:String , required:true , min:2 , max:30},
-    email:{type:String , required: true},
-    phone:{type:String , required:true},
-    link1:{type:String , required:false},
-    link2:{type:String , required:false},
-    link3:{type:String , required:false}
-})
+  name: { type: String, required: true, min: 2, max: 30 },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  link1: { type: String, required: false },
+  link2: { type: String, required: false },
+  link3: { type: String, required: false },
+});
 
 const statementSchema = new Schema({
-    header:{type:String, required:false , min:3, max:50},
-    body:{type:String , required:false , min:[10,'Min char length is 10.'] , max:[300 , 'Max char length is 300.'] }
-})
+  header: { type: String, required: false, min: 3, max: 50 },
+  body: {
+    type: String,
+    required: false,
+    min: [10, 'Min char length is 10.'],
+    max: [300, 'Max char length is 300.'],
+  },
+});
 
 const skillSchema = new Schema({
-    skill:{type:String , required:true , min:1 , max:[20,'Max skill chars of 20.']},
-    priority:{type:Number, required:true , min:0,max:2, default:0}
-})
-
-const sectionSchema = new Schema({
-    cond: conditionSchema,
-    header:{type:String , required:true , min:[3, 'Section header too short. Min 3.'] , max:[20,'Section header too long. Max 20.']},
-    subsections: [subSectionSchema],
-})
+  skill: {
+    type: String,
+    required: true,
+    min: 1,
+    max: [20, 'Max skill chars of 20.'],
+  },
+  priority: { type: Number, required: true, min: 0, max: 2, default: 0 },
+});
 
 const conditionSchema = new Schema({
-    priority: {type:Number , required:true, default:0 , min:0 , max:2},
-    items:{type:Number , required:true , min:1 , max:5},
-})
-
-const subSectionSchema = new Schema({
-    cond:conditionSchema,
-    subheader:{type:String , required:true , min:[3, 'SubSection header too short. Min 3.'] , max:[40,'SubSection header too long. Max 40.']},
-    dateStart:{type:Date, required:false },
-    dateEnd:{type:Date, required:false},
-    lines:[lineSchema]
-})
+  priority: { type: Number, required: true, default: 0, min: 0, max: 2 },
+  items: { type: Number, required: true, min: 1, max: 5 },
+});
 
 const lineSchema = new Schema({
-    priority:{type:Number , required:true, default:0 , min:0 , max:2},
-    body:{type:String , required:true, min:[5 , 'Line must be at least 5 characters.'] , max:[80, 'Line cannot exceed 80 characters.']},
-    tags:[{type:String}]
-})
+    priority: { type: Number, required: true, default: 0, min: 0, max: 2 },
+    body: {
+      type: String,
+      required: true,
+      min: [5, 'Line must be at least 5 characters.'],
+      max: [80, 'Line cannot exceed 80 characters.'],
+    },
+    tags: [{ type: String }],
+  });
 
-module.exports = mongoose.model('Resume' , resumeSchema)
+const subSectionSchema = new Schema({
+  cond: conditionSchema,
+  subheader: {
+    type: String,
+    required: true,
+    min: [3, 'SubSection header too short. Min 3.'],
+    max: [40, 'SubSection header too long. Max 40.'],
+  },
+  dateStart: { type: Date, required: false },
+  dateEnd: { type: Date, required: false },
+  lines: [lineSchema],
+});
+
+const sectionSchema = new Schema({
+  cond: conditionSchema,
+  header: {
+    type: String,
+    required: true,
+    min: [3, 'Section header too short. Min 3.'],
+    max: [20, 'Section header too long. Max 20.'],
+  },
+  subsections: [subSectionSchema],
+});
+
+const resumeSchema = new Schema({
+  personal: personalSchema,
+  statement: { type: statementSchema, required: false },
+  skills: {
+    type: [{ type: skillSchema, required: false, min: 5, max: 20 }],
+    // validate: [50, 'Too many skills. Max 50'],
+  },
+  projects: sectionSchema,
+  workHistory: sectionSchema,
+  education: sectionSchema,
+});
+
+
+
+module.exports = mongoose.model('Resume', resumeSchema);
