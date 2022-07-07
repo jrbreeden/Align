0898e903ed258d4a33d6afd4f43e440ced1ca457
Jhimson <jhimsonrayraboypamisa@gmail.com>
2022-07-07 09:12:10 -0400

    Created team page component and added to homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import Carousel from '../../components/Carousel/Carousel';
 import Layout from '../../components/Layout/Layout';
-import resumeConstructor from './ResumeConstructor'
+import Team from '../../components/Team/Team';
+import resumeConstructor from './ResumeConstructor';
 
-const testData =require('./sampleUser')
+const testData = require('./sampleUser');
 
 export default function HomePage({ user }) {
-  console.log('test data is ', testData)
+  console.log('test data is ', testData);
   function handleClick() {
     resumeConstructor(testData);
   }
   return (
     <Layout active={'home'} user={user}>
       <button onClick={handleClick}>Download Resume</button>
-      <div className="h-screen">
-        <Carousel />
+      <div className="MAIN DIV flex flex-col gap-y-72">
+        <div className="CAROUSEL">
+          <Carousel />
+        </div>
+        <div className="TEAMS">
+          <Team />
+        </div>
       </div>
     </Layout>
   );
