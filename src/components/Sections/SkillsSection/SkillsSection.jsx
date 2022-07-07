cb79a1129c15f2c759aea0b7ba1e2a9416073680
Jhimson <jhimsonrayraboypamisa@gmail.com>
2022-07-07 06:44:01 -0400

    Added handleSubmit in Skills section

--- src/components/Sections/SkillsSection/SkillsSection.jsx
+++ src/components/Sections/SkillsSection/SkillsSection.jsx
@@ -15,6 +15,19 @@ export default function PersonalInfoSection({
       [e.target.name]: e.target.value,
     }));
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSkills((prevSkills) => [
+      ...prevSkills,
+      {
+        id: uuidv4(),
+        skill: skill.skill,
+        priority: parseInt(skill.priority),
+      },
+    ]);
+    setSkill({ id: '', skill: '', priority: 0 });
+  };
   return (
     <Spring
       from={{ opacity: 0, marginLeft: -1000 }}
@@ -33,55 +46,47 @@ export default function PersonalInfoSection({
               </ul>
 
               <div className="w-96">
-                <div className="my-2 mt-8">
-                  <input
-                    className="shadow appearance-none border rounded w-5/6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="skill"
-                    type="text"
-                    name="skill"
-                    value={skill.skill}
-                    onChange={handleChange}
-                    placeholder="Enter Skill"
-                  />
-                </div>
-                <div className="inline-block relative w-48 mr-2 mt-2">
-                  <select
-                    className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline text-gray-500"
-                    name="priority"
-                    value={skill.priority}
-                    onChange={handleChange}
-                  >
-                    <option value={0}>Normal</option>
-                    <option value={1}>Essential</option>
-                    <option value={2}>High Priority</option>
-                  </select>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg
-                      className="fill-current h-4 w-4"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                    </svg>
+                <form onSubmit={handleSubmit}>
+                  <div className="my-2 mt-8">
+                    <input
+                      className="shadow appearance-none border rounded w-5/6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id="skill"
+                      type="text"
+                      name="skill"
+                      value={skill.skill}
+                      onChange={handleChange}
+                      placeholder="Enter Skill"
+                      required={true}
+                    />
                   </div>
-                </div>
-                <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  type="button"
-                  onClick={() => {
-                    setSkills((prevSkills) => [
-                      ...prevSkills,
-                      {
-                        id: uuidv4(),
-                        skill: skill.skill,
-                        priority: parseInt(skill.priority),
-                      },
-                    ]);
-                    setSkill({ id: '', skill: '', priority: 0 });
-                  }}
-                >
-                  Add Skill
-                </button>
+                  <div className="inline-block relative w-48 mr-2 mt-2">
+                    <select
+                      className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline text-gray-500"
+                      name="priority"
+                      value={skill.priority}
+                      onChange={handleChange}
+                    >
+                      <option value={0}>Normal</option>
+                      <option value={1}>Essential</option>
+                      <option value={2}>High Priority</option>
+                    </select>
+                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                      <svg
+                        className="fill-current h-4 w-4"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="submit"
+                  >
+                    Add Skill
+                  </button>
+                </form>
               </div>
             </div>
             <SkillsView
