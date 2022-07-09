1015ba9e5d645b4cb77f0c864cbc5c3fbde42be6
Jhimson <jhimsonrayraboypamisa@gmail.com>
2022-07-09 13:30:17 -0400

    Bug fix: error inserting resume

--- controllers/api/C-resume.js
+++ controllers/api/C-resume.js
@@ -1,34 +1,40 @@
 const Resume = require('../../models/M-resume');
 
 const addNewResume = async (req, res) => {
+  req.body.skills = req.body.skills.map((skill) => {
+    skill = {skill: skill.skill, priority: skill.priority };
+    return skill;
+  });
+  console.log(req.body);
   let newResume;
-  Resume.deleteOne({user: req.body.user}).then((del)=>{
-    console.log('deleted ' , del)
+
+  Resume.deleteOne({ user: req.body.user }).then((del) => {
+    console.log('deleted ', del);
     Resume.create(req.body)
-    .then((response) => {
-      newResume = response;
-      console.log('neewwResume', newResume);
-      res.status(200);
-  })
-    .catch((err) => {
-      console.log(err);
-    });
-  })
-  
+      .then((response) => {
+        newResume = response;
+        console.log('neewwResume', newResume);
+        res.status(200);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 };
 
-const getResume = async(req,res)=>{
-  console.log('finding resume for id , ' ,req.body.id)
-  Resume.findOne({user:req.body.id}).then((doc)=>{
-    console.log('returned the users resume from c-res as ,' , doc)
-    res.json(doc)
-  }).catch((err)=>{
-    console.log('Error retreiving user resume from c-resume' , err)
-  })
-}
-
+const getResume = async (req, res) => {
+  console.log('finding resume for id , ', req.body.id);
+  Resume.findOne({ user: req.body.id })
+    .then((doc) => {
+      console.log('returned the users resume from c-res as ,', doc);
+      res.json(doc);
+    })
+    .catch((err) => {
+      console.log('Error retreiving user resume from c-resume', err);
+    });
+};
 
 module.exports = {
   addNewResume,
-  getResume
+  getResume,
 };
--- src/pages/ConstructionPage/ConstructionPage.jsx
+++ src/pages/ConstructionPage/ConstructionPage.jsx
@@ -298,6 +298,7 @@ export default function ConstructionPage({ user }) {
           ...prevState,
           ...test.education.subSections,
         ]);
+        console.log(resume)
       }
     })();
   }, []);
