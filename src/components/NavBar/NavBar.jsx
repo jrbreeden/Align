65ada264af59be28ca05e73ec1c252b08f5afb5b
Jhimson <jhimsonrayraboypamisa@gmail.com>
2022-07-10 07:09:24 -0400

    Re-design navbar - initial

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,194 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../../assets/images/Align6.png'
+import Logo from '../../assets/images/Align6.png';
+import sample from '../../assets/images/sample.jpg';
 import { logOut } from '../../utilities/users-service';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function NavBar({ active, user, setUser }) {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [currentPath, setCurrentPath] = useState('');
   const handleLogOut = () => {
     logOut();
     setUser(null);
     navigate('/login');
+    setIsShow(false);
   };
+
+  const [isShow, setIsShow] = useState(false);
+
+  useEffect(() => {
+    console.log(location);
+    setCurrentPath(location.pathname);
+    console.log('wtf', currentPath);
+  }, [location]);
 
   return (
     <>
-      <nav className="bg-gray-800" style={{ height: '10vh' }}>
+      <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+        <div class="container flex flex-wrap justify-between items-center mx-auto">
+          <div className="flex gap-x-10">
+            <Link to="/profle" class="flex items-center">
+              <img src={Logo} class="mr-3 h-6 sm:h-9" alt="" />
+            </Link>
+            {user && (
+              <div
+                class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
+                id="mobile-menu-2"
+              >
+                <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                  <li>
+                    <Link
+                      to="/profile"
+                      class={`${
+                        location.pathname === '/profile' && 'bg-red-200'
+                      } block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}
+                    >
+                      Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/construction"
+                      class={`${
+                        location.pathname === '/construction' && 'bg-red-200'
+                      } block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}
+                    >
+                      Resume Builder
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/featured-jobs"
+                      class={`${
+                        location.pathname === '/featured-jobs' && 'bg-red-200'
+                      } block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}
+                    >
+                      Search Jobs
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+
+          <div className="left nav-items">
+            {!user && (
+              <div
+                class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
+                id="mobile-menu-2"
+              >
+                <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                  <li>
+                    <Link
+                      to="/signup"
+                      class={`${
+                        location.pathname === '/signup' && 'bg-red-200'
+                      } block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}
+                    >
+                      Signup
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/login"
+                      class={`${
+                        location.pathname === '/login' && 'bg-red-200'
+                      } block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}
+                    >
+                      Login
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+            <div class={`${!user && 'hidden'}flex items-center md:order-2`}>
+              <button
+                type="button"
+                class={`${
+                  !user && 'hidden'
+                } flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600`}
+                id="user-menu-button"
+                aria-expanded="false"
+                data-dropdown-toggle="dropdown"
+                onClick={() => setIsShow(!isShow)}
+              >
+                <span class="sr-only">Open user menu</span>
+                <img
+                  class="w-8 h-8 rounded-full"
+                  src={sample}
+                  alt="user photo"
+                />
+              </button>
+
+              {/* <!-- Dropdown menu --> */}
+              <div
+                class={`${
+                  !isShow && 'hidden'
+                } z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 absolute`}
+                id="dropdown"
+              >
+                <div class="py-3 px-4">
+                  <span class="block text-sm text-gray-900 dark:text-white">
+                    {user && user.name}
+                  </span>
+                  <span class="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">
+                    jim@gmail.com
+                  </span>
+                </div>
+                <ul class="py-1" aria-labelledby="dropdown">
+                  <li>
+                    <button
+                      class="w-full block py-2 px-4 text-sm text-red-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-red-500 dark:hover:text-white"
+                      onClick={handleLogOut}
+                    >
+                      Sign out
+                    </button>
+                  </li>
+                </ul>
+              </div>
+              <button
+                data-collapse-toggle="mobile-menu-2"
+                type="button"
+                class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                aria-controls="mobile-menu-2"
+                aria-expanded="false"
+              >
+                <span class="sr-only">Open main menu</span>
+                <svg
+                  class="w-6 h-6"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  class="hidden w-6 h-6"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+      {/* <nav className="bg-gray-800" style={{ height: '10vh' }}>
         <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8" style={{ height: '10vh' }}>
           <div className="relative flex items-center justify-between h-16" style={{ height: '10vh' }}>
             {active ?
@@ -23,7 +196,7 @@ export default function NavBar({ active, user, setUser }) {
                 <div className="flex-shrink-0 flex items-center">
                   <Link to='/'><img
                     className="hidden lg:block w-auto"
-                    style={{ height: '11vh' }}
+                    style={{ height: '8vh' }}
                     src={Logo}
                     alt="Align"
                   /></Link>
@@ -116,7 +289,7 @@ export default function NavBar({ active, user, setUser }) {
             </a>
           </div>
         </div>
-      </nav>
+      </nav> */}
     </>
   );
 }
