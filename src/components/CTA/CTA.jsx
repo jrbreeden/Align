f91763c5554c3a48adfcb191dacdd712895d8742
bhenderson93 <brycehenderson1993@gmail.com>
2022-07-10 21:36:28 -0700

    final touches

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -22,15 +22,13 @@ const CTA = () => {
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">Let's make your Job Search</span>{' '}
+                <span className="block xl:inline">Let us handle the details, </span>{' '}
                 <span className="block text-indigo-600 xl:inline">
-                  Like A Piece of Cake
+                  you just be you!
                 </span>
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-                fugiat aliqua.
+                Don't worry about presenting yourself 10 different ways to 10 different employers. We'll take care of that!
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
@@ -41,15 +39,7 @@ const CTA = () => {
                     Get started{' '}
                   </Link>
                 </div>
-                <div className="mt-3 sm:mt-0 sm:ml-3">
-                  <a
-                    href="#"
-                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
-                  >
-                    {' '}
-                    Live demo{' '}
-                  </a>
-                </div>
+
               </div>
             </div>
           </main>
--- src/utilities/helpers/CarouselData.js
+++ src/utilities/helpers/CarouselData.js
@@ -1,6 +1,6 @@
-import myResume from "../../assets/images/myResume.jpg"
-import Computer from "../../assets/images/Computer.jpg"
-import forHire from "../../assets/images/forHire.jpg"
+import myResume from "./myResume.jpg"
+import Computer from "./Computer.jpg"
+import forHire from "./forHire.jpg"
 export const images = [
     {title: "About us", subtitle: "Are you tired of searching for jobs? We have an agent who can search for jobs for you", img: myResume },
     {title: "Our Offering", subtitle: "We offer job assistance and resume help based on your skillset", img: Computer },
