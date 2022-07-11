e5cb58c44cdc671d8d2db9b4f97d40f54579d8ee
Jhimson <jhimsonrayraboypamisa@gmail.com>
2022-07-11 00:31:48 -0400

    Fix welcome message to not show when user is logged out

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -148,18 +148,20 @@ export default function NavBar({ active, user, setUser }) {
                     />
                   </button>
                 </div>
-                <div className="">
-                  <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-                    <li
-                      className={`text-white bg-none ${
-                        location.pathname === '/login' &&
-                        'bg-blue-800 px-4 py-2 rounded'
-                      }`}
-                    >
-                      {user && `Welcome, ${user.name}`}
-                    </li>
-                  </ul>
-                </div>
+                {user && (
+                  <div className="">
+                    <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                      <li
+                        className={`text-white bg-none ${
+                          location.pathname === '/login' &&
+                          'bg-blue-800 px-4 py-2 rounded'
+                        }`}
+                      >
+                        {user && `Welcome, ${user.name}`}
+                      </li>
+                    </ul>
+                  </div>
+                )}
               </div>
 
               {/* <!-- Dropdown menu --> */}
