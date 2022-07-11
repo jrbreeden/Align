2ab49d62b29af585413cf43c68a441e141255690
bhenderson93 <brycehenderson1993@gmail.com>
2022-07-10 21:12:20 -0700

    made it prettier

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -49,7 +49,7 @@ export default function AppliedJobsPage({ user , setUser, getUser, markAsApplied
           <div className="flex flex-col w-full justify-center items-center">
             <div className="p-16">
             <button 
-            className="btn inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center" 
+            className="btn inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center justify-center" 
             onClick={handleClick}
             style={{height:'10vh' , width:'20vw', fontSize:'2vh'}}>Download Master Resume</button>
               </div>
